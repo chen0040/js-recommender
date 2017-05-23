84dbd9778080892d2f02dd43813df829916763cb
xschen <xs0040@gmail.com>
2017-05-23 16:04:00 +0800

    Table created

--- src/jsrecommender.js
+++ src/jsrecommender.js
@@ -1,7 +1,62 @@
 var jsrecommender = jsrecommender || {};
 
 (function(jss){
-	var Recommender = function(){
+    var Table = function() {
+        this.rowNames = [];
+        this.columnNames = [];
+        this.cells = {};
+        this.cellCount = 0;
+    };
+    
+    Table.prototype.cellKey = function(rowName, colName) {
+        return rowName + '-' + colName;  
+    };
+    
+    Table.prototype.addRowIfNotExists = function(rowName) {
+        for(var i = 0; i < this.rowNames.length; ++i) {
+            if(this.rowNames[i] == rowName) {
+                return;
+            }
+        }  
+        this.rowNames.push(rowName);
+    };
+    
+    Table.prototype.addColumnIfNotExists = function(colName) {
+        for(var i = 0; i < this.columnNames.length; ++i) {
+            if(this.columnNames[i] == colName) {
+                return;
+            }
+        }
+        this.columnNames.push(colName);
+    }
+    
+    Table.prototype.setCell = function(rowName, colName, value) {
+        var key = this.cellKey(rowName, colName);
+        var exists = this.containsCell(rowName, colName);
+        this.cells[key] = value;
+        if(!exists) {
+            this.cellCount++;
+            this.addRowIfNotExists(rowName);
+            this.addColumnIfNotExists(colName);
+        }
+    };
+    
+    Table.prototype.containsCell = function(rowName, colName) {
+        var key = this.cellKey(rowName, colName);
+        return key in this.cells;
+    };
+    
+    Table.prototype.removeCell = function(rowName, colName) {
+        var exists = this.containsCell(rowName, colName);
+        if(exists){
+            this.cellCount--;
+            delete this.cells[this.cellKey(rowName, colName)];
+        }
+    };
+    
+    jss.Table = Table;
+    
+	var Recommender = function(table){
         
     }
 
---
+++ test/table-spec.js
@@ -0,0 +1,24 @@
+var expect    = require("chai").expect;
+var jsrecommender = require("../src/jsrecommender");
+
+describe("Table api test", function() {
+  describe("Create and add values to table", function() {
+    var table = new jsrecommender.Table();
+    table.setCell('movie1', 'user1', 5);
+    table.setCell('movie2', 'user1', 4.5);
+    table.setCell('movie1', 'user2', 3.0);
+      
+    it("has two columns", function() {
+    	expect(table.columnNames.length).to.equal(2);
+        expect(table.columnNames[0]).to.equal('user1');
+        expect(table.columnNames[1]).to.equal('user2');
+    });
+    it("has two rows", function() {
+    	expect(table.rowNames.length).to.equal(2);
+        expect(table.rowNames[0]).to.equal('movie1');
+        expect(table.rowNames[1]).to.equal('movie2');
+    });
+  });
+
+
+});
