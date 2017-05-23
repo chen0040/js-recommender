a53f3c88650ab493edc79878a130bbfbfce41d34
xschen <xs0040@gmail.com>
2017-05-23 15:03:53 +0800

    project setup

---
+++ src/jsrecommender.js
@@ -0,0 +1,14 @@
+var jsrecommender = jsrecommender || {};
+
+(function(jss){
+	var Recommender = function(){
+        
+    }
+
+    jss.Recommender = Recommender;
+
+})(jsrecommender);
+
+if(module) {
+	module.exports = jsrecommender;
+}
